d426d976f3b5694b4bceab87d61632a72c0e422e
João Victor Nogueira <142459887+jvdesigner@users.noreply.github.com>
2023-09-10 10:33:35 +0000

    update

--- index.js
+++ index.js
@@ -25,14 +25,19 @@ import * as elementos from "./variaveis/elementos.js";
 elementos.toggle.addEventListener('change',()=>funcoes.efeitoDarkmode());
 
 if (localStorage.getItem('darkMode') === 'true') {
+
     elementos.toggle.checked  = true;
 
 } else {
+
     document.querySelector('html').classList.remove('dark');
+
     elementos.toggle.checked  = false;
 }
 
 document.querySelector('html').style.display="block";
+
+AOS.init();
 
 // ------------------------------------------------------------------------
 
